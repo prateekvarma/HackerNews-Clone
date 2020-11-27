ca19dffe927990d591216b41c2b2a11c7cd3d39a
Prateek Varma <prtk.varma@gmail.com>
2020-11-27 13:46:49 -0600

    added create user mutation config  to main schema

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -1,13 +1,14 @@
 import graphene
 
 import links.schema
+import users.schema
 
 #get data
 class Query(links.schema.Query, graphene.ObjectType):
     pass
 
 #insert data
-class Mutation(links.schema.Mutation, graphene.ObjectType):
+class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
