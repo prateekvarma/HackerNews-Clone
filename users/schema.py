0f481de5bb3d030b8c8c6e7fdabbf373fac7e26b
Prateek Varma <prtk.varma@gmail.com>
2020-11-27 15:30:23 -0600

    added test query to check JWT and return user

--- users/schema.py
+++ users/schema.py
@@ -33,3 +33,19 @@ class Query(graphene.ObjectType):
 
     def resolve_users(self, info):
         return get_user_model().objects.all()
+
+#testing to authenticate user
+class Query(graphene.AbstractType):
+    me = graphene.Field(UserType)
+    users = graphene.List(UserType)
+
+    def resolve_users(self, info):
+        return get_user_model().objects.all()
+
+    def resolve_me(self, info):
+        user = info.context.user
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
+
+        return user
+#end testing authenticate user
